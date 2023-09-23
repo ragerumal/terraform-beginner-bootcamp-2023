704245988fbe61fce08aef8dbfc3dd2cb2a0875f
raghunath erumal <raghunath.e@gmail.com>
2023-09-23 14:21:00 +0000

    Provide example env for env project root , provide good documentation about env vars

--- bin/install_terraform_cli.sh
+++ bin/install_terraform_cli.sh
@@ -1,6 +1,8 @@
 
 #!/usr/bin/env bash
 
+cd /workspace
+
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 
 wget -O- https://apt.releases.hashicorp.com/gpg | \
@@ -18,3 +20,5 @@ sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 
 sudo apt-get install terraform -y
+
+cd $PROJECT_ROOT
