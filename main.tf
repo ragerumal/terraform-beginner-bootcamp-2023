da9dae04dd7ad596f499077abb8ccf09233d1b04
raghunath erumal <raghunath.e@gmail.com>
2023-09-28 07:20:54 +0000

    @25 Refactor S3 bucket in to new Terrahouse module

--- main.tf
+++ main.tf
@@ -1,11 +1,27 @@
+terraform {
+  # backend "remote" {
+  #   hostname = "app.terraform.io"
+  #   organization = "TerraformTrainingRaghuerumal"
 
+  #   workspaces {
+  #     name = "terra-house-1"
+  #   } 
 
-
-
-resource "aws_s3_bucket" "website_bucket" {
-bucket = var.bucket_name
-  tags = {
-      UserUuid = var.user_uuid
-    }
+#   cloud {
+#     organization = "TerraformTrainingRaghuerumal"
+#     workspaces {
+#       name = "terra-house-1"
+#     }
+#   }
+  
 }
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid= var.user_uuid
+  bucket_name = var.bucket_name
+}
+
+
+
 
--- variables.tf
+++ variables.tf
@@ -1,17 +1,9 @@
 variable "user_uuid" {
-  description = "User UUID"
   type        = string
-  validation {
-    condition     = can(regex("^([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$", var.user_uuid))
-    error_message = "Invalid User UUID format. It should be in the form of a UUID (e.g., 123e4567-e89b-12d3-a456-426655440000)."
-  }
+
 }
 
 variable "bucket_name" {
-  description = "AWS S3 Bucket Name"
   type        = string
-  validation {
-    condition     = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
-    error_message = "Invalid S3 bucket name. It should be between 3 and 63 characters long and only contain lowercase letters, numbers, hyphens, and periods."
-  }
+
 }
