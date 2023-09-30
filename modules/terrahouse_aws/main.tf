5557a5efd6feb7bd58d4f9837cdcb1a8b854227a
ragerumal <126337647+ragerumal@users.noreply.github.com>
2023-09-30 09:18:10 -0700

    #29 create CDN and serve s3 bucket from cdn (#30)

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -15,45 +15,7 @@ terraform {
 # #   access_key = "my-access-key"
 # #   secret_key = "my-secret-key"
 # }
-resource "aws_s3_bucket" "website_bucket" {
-bucket = var.bucket_name
-  tags = {
-      UserUuid = var.user_uuid
-    }
-}
 
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
-resource "aws_s3_bucket_website_configuration" "website_configuration" {
-  bucket = aws_s3_bucket.website_bucket.bucket
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
 
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = var.index_html_filepath
-
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-    etag = "${md5(file(var.index_html_filepath))}"
-  #etag = filemd5("path/to/file")
-}
-
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  source = var.error_html_filepath
-
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-    etag = "${md5(file(var.error_html_filepath))}"
-  #etag = filemd5("path/to/file")
-}
+data "aws_caller_identity" "current" {}
