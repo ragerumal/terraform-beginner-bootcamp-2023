fc50a3aaf8cbc4c03ede8342ebbd6058e0e83a8e
raghunath erumal <raghunath.e@gmail.com>
2023-10-05 07:43:13 +0000

    comment out code and make things work

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,10 @@ output "bucket_name" {
     description = "S3 static website hosting endpoint"
     value = module.terrahouse_aws.website_endpoint
  }
+
+ locals {
+   root_path=path.root
+ }
+ output "root_path" {
+   value = local.root_path
+ }
