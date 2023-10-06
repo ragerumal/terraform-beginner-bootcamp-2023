8c94a1fe4ce111b38dffb87bc4392389df3cde0b
raghunath erumal <raghunath.e@gmail.com>
2023-10-06 13:32:33 +0000

    revert chnages

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "website_bucket" {
 bucket = var.bucket_name
   tags = {
       UserUuid = var.user_uuid
-      Hello = "Uranus"
+      Hello = "Neptune"
     }
 }
 
@@ -22,27 +22,27 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = "${path.root}${var.index_html_filepath}"
-  content_type = "text/html"
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-    etag = "${md5(file("${path.root}${var.index_html_filepath}"))}"
-  #etag = filemd5("path/to/file")
-}
+# resource "aws_s3_object" "index_html" {
+#   bucket = aws_s3_bucket.website_bucket.bucket
+#   key    = "index.html"
+#   source = "${path.root}${var.index_html_filepath}"
+#   content_type = "text/html"
+#   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+#     etag = "${md5(file("${path.root}${var.index_html_filepath}"))}"
+#   #etag = filemd5("path/to/file")
+# }
 
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  source = "${path.root}${var.error_html_filepath}"
-  content_type = "text/html"
+# resource "aws_s3_object" "error_html" {
+#   bucket = aws_s3_bucket.website_bucket.bucket
+#   key    = "error.html"
+#   source = "${path.root}${var.error_html_filepath}"
+#   content_type = "text/html"
 
 
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-    etag = "${md5(file("${path.root}/${var.error_html_filepath}"))}"
-  #etag = filemd5("path/to/file")
-}
+#   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+#     etag = "${md5(file("${path.root}/${var.error_html_filepath}"))}"
+#   #etag = filemd5("path/to/file")
+# }
 
 #https://aws.amazon.com/blogs/networking-and-content-delivery/amazon-cloudfront-introduces-origin-access-control-oac/
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy
