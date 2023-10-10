5d88dc33909fe8d96cad84421604e1ebf8ff9806
raghunath erumal <raghunath.e@gmail.com>
2023-10-10 14:10:10 +0000

    #42 second commit

---
+++ terraform-provider-terratowns/main.go
@@ -0,0 +1,10 @@
+//It specifies that it belongs to the main package. In Go, the main package is the entry point for the program.
+package main
+//It imports the fmt package, which provides functions for formatting and printing.
+import "fmt"
+//It defines the main function, which is the entry point for the program.
+
+//Inside the main function, it uses the fmt.Println function to print "Hello, World!" to the console.
+func main() {
+    fmt.Println("Hello, World!")
+}
