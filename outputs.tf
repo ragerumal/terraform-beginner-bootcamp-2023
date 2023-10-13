b03eaa2ba3227fdeafea22be38178ab38985bc8e
ragerumal <126337647+ragerumal@users.noreply.github.com>
2023-10-12 20:07:41 -0700

    #47 test our terra home on terrotowns (#48)

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-# output "bucket_name" {
-#     description = "bucket name for our static website"
-#     value = module.terrahouse_aws.bucket_name
-#  }
-#  output "s3_website_endpoint" {
-#     description = "S3 static website hosting endpoint"
-#     value = module.terrahouse_aws.website_endpoint
-#  }
+output "bucket_name" {
+    description = "bucket name for our static website"
+    value = module.terrahouse_aws.bucket_name
+ }
+ output "s3_website_endpoint" {
+    description = "S3 static website hosting endpoint"
+    value = module.terrahouse_aws.website_endpoint
+ }
 
  
-# output "cloudfront_url" {
-#   description = "Cloudfront distribution domain name"
-#   value = module.terrahouse_aws.cloudfront_url
-# }
+output "cloudfront_url" {
+  description = "Cloudfront distribution domain name"
+  value = module.terrahouse_aws.cloudfront_url
+}
